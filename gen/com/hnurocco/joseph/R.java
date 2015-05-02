4c335b508187867173be8eec0532472fa3c93e6b
Rocco <roccowang95@gmail.com>
2015-05-03 01:55:05 +0800

    drawer made

--- gen/com/hnurocco/joseph/R.java
+++ gen/com/hnurocco/joseph/R.java
@@ -21,19 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_chevron_left_24px=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int Calculate=0x7f080004;
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f08000a;
+        public static final int content_frame=0x7f080007;
+        public static final int drawer_layout=0x7f080006;
         public static final int input_m=0x7f080003;
         public static final int input_n=0x7f080002;
+        public static final int left_drawer=0x7f080008;
         public static final int linearLayout1=0x7f080000;
+        public static final int list_item=0x7f080009;
         public static final int out=0x7f080005;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int drawer=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/hnurocco/joseph/MainActivity.java
+++ src/com/hnurocco/joseph/MainActivity.java
@@ -3,60 +3,86 @@ package com.hnurocco.joseph;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.LauncherActivity.ListItem;
+import android.support.v4.widget.DrawerLayout;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Adapter;
 
 public class MainActivity extends Activity {
 
-	@SuppressWarnings("static-access")
+	private String[] hahaha = {"fffff","ffffdsa","fdsa"};
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	
+	private class DrawerItemClickListener{
+		public void OnItemClick(AdapterView parent, View view, int position, long id){
+			selectItem(position);
+		}
+	}
+	private void selectItem(int position){
+		Fragment fragment = new Fragment();
+		Bundle args = new Bundle();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button b = (Button)findViewById(R.id.Calculate);
-		final EditText in_n = (EditText)findViewById(R.id.input_n);
-		final EditText in_m = (EditText)findViewById(R.id.input_m);
-		final TextView out = (TextView)findViewById(R.id.out);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.id.list_item, hahaha));
+		
+
+		Button b = (Button) findViewById(R.id.Calculate);
+		final EditText in_n = (EditText) findViewById(R.id.input_n);
+		final EditText in_m = (EditText) findViewById(R.id.input_m);
+		final TextView out = (TextView) findViewById(R.id.out);
 		out.setMovementMethod(new ScrollingMovementMethod());
-		final String tn = in_n.getText().toString();
-		final String tm = in_n.getText().toString();
 		b.setOnClickListener(new OnClickListener() {
 			@SuppressLint("NewApi")
 			@Override
 			public void onClick(View v) {
-				if((in_n.getText().toString().isEmpty())||(in_m.getText().toString().isEmpty())){
-					
-					Toast t =Toast.makeText(MainActivity.this, "Please enter item", Toast.LENGTH_SHORT);
-				    t.show();
-					
-				}
-				else{
+				if ((in_n.getText().toString().isEmpty())
+						|| (in_m.getText().toString().isEmpty())) {
+
+					Toast t = Toast.makeText(MainActivity.this,
+							"Please enter item", Toast.LENGTH_SHORT);
+					t.show();
+
+				} else {
 					out.setText("");
 					int num = Integer.parseInt(in_n.getText().toString());
 					int step = Integer.parseInt(in_m.getText().toString());
-					int[] ring= new int[num];
+					int[] ring = new int[num];
 					for (int tj = 0; tj < num; tj++)
 						ring[tj] = tj + 1;
-					ring[num-1] = 0;
+					ring[num - 1] = 0;
 					int x = num - 1;
 					String s;
-					while(ring[x]!=x){
+					while (ring[x] != x) {
 						int tj = step - 1;
-						while(tj-->0){
+						while (tj-- > 0) {
 							x = ring[x];
 						}
-						out.append(String.valueOf(ring[x]+1) + " ");
+						out.append(String.valueOf(ring[x] + 1) + " ");
 						ring[x] = ring[ring[x]];
 						out.append("\n");
 					}
-					out.append(String.valueOf(x+1) + " ");
-					
+					out.append(String.valueOf(x + 1) + " ");
+
 				}
 			}
 		});
